be9e961604428cea3c47d746658c98a15b6ea2b7
csc2216 <csc.jpln@gmail.com>
2026-07-06 21:23:36 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> lp, wait_for;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+
+            if (c == '(' || c == '{' || c == '[') {
+                lp.push(c);
+
+                if (c == '(') wait_for.push(')');
+                    else if (c == '{') wait_for.push('}');
+                        else wait_for.push(']');
+
+            } else if (wait_for.empty()) {
+                return false;
+            } else if (c == wait_for.top()) {
+                lp.pop();
+                wait_for.pop(); 
+            } else return false;
+        }
+
+        if (lp.empty()) return true;
+            else return false;
+    }
+};
