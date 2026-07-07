c8c7fa7c7577a2c07e3782ffbda1bd6e1b8f430a
Ethan Chen <csc.jpln@gmail.com>
2026-07-07 15:29:11 +0800

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+        int mid = (l + r) / 2;
+
+        while (l <= r) {
+            if (target == nums[mid]) return mid;
+            if (target > nums[mid]) l = mid + 1;
+            if (target < nums[mid]) r = mid - 1;
+            mid = (l + r) / 2;   
+        }
+
+        return -1;
+    }
+};
